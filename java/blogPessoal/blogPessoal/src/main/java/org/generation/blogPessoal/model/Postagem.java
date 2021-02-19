c1cfcad8cce70cd2ba6caa89a41b422791a60a5e
Victor Araujo <Victor>
2021-02-19 14:50:59 -0300

    BlogPessoal

--- java/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
+++ java/blogPessoal/blogPessoal/src/main/java/org/generation/blogPessoal/model/Postagem.java
@@ -6,11 +6,15 @@ import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 import javax.persistence.Table;
 import javax.persistence.Temporal;
 import javax.persistence.TemporalType;
 import javax.validation.constraints.Size;
 
+import org.hibernate.annotations.ManyToAny;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.sun.istack.NotNull;
 
 @Entity // indica essa classe vai ser uma entidade do jpa hibernate
@@ -26,7 +30,7 @@ public class Postagem {
 	@NotNull // para ser obrigatorio o preenchimento do campo
 	@Size(min = 5, max = 100) // definir a qtd de caracter
 	private String titulo;
-	
+
 	@NotNull
 	@Size(min = 10, max = 500)
 	private String texto;
@@ -35,6 +39,10 @@ public class Postagem {
 	// calcular a data que foi postada com um metodo
 	@Temporal(TemporalType.TIMESTAMP) // dizer para o jpa hibertante que estamos trabalhando com o tempo
 	private Date date = new java.sql.Date(System.currentTimeMillis());
+
+	@ManyToOne
+	@JsonIgnoreProperties("postagem")
+	private Tema tema;
 
 	// inserir os getters e setters
 	public long getId() {
@@ -69,4 +77,11 @@ public class Postagem {
 		this.date = date;
 	}
 
+	public Tema getTema() {
+		return tema;
+	}
+
+	public void setTema(Tema tema) {
+		this.tema = tema;
+	}
 }
